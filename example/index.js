f3cdfa73dedb7f8441612edf40fde85c1cfa0d58
STS\midhun.gs <midhun.gs@6dtech.co.in>
2019-01-08 16:23:49 +0530

    Added Example

---
+++ example/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const validator = require('./validator');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/test',
+validator([
+  {param : 'page', location : 'body', isObject : true, children : [
+    {param : 'sorted', location : 'body.page', isRequired : true, isBoolean : true},
+  ]},
+  {param : 'sort', location : 'body', isArray : true, children : [
+    {param : 'a', location : 'body.sort', isArray : true, children : [
+      {param : 'sort', location : 'body.sort.a', isRequired : true, isDate : true},
+      {param : 'pageNumber', location : 'body.sort.a', isObject : true, children : [
+        {param : 'b', location : 'body.sort.a.pageNumber', isNumber : true},
+        {param : 'c', location : 'body.sort.a.pageNumber', isNumber : true}
+      ]},
+      {param : 'pageNumber1', location : 'body.sort.a', isObject : true, children : [
+        {param : 'd', location : 'body.sort.a.pageNumber1', isNumber : true},
+        {param : 'e', location : 'body.sort.a.pageNumber1', isNumber : true}
+      ]}
+    ]}
+  ]}
+],
+{ mode : 'reject', errorCode : '422' }),
+(req, res, next) => {
+  if (res.locals.statusCode === errorCode.Field.Invalid_Field)
+    console.log("Invalid Field Status Code");
+
+  console.log("Exit");
+  res.status(200)
+
+});
+
+//Starting app
+app.listen(5000, err => {
+  if (err) {
+    console.error(`Error While Starting app Error :: ${err}`);
+  }
+  console.log(`server is listening on 5000`);
+});
