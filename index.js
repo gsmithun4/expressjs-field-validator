87e09e962089e3215dabfa8a980851f0a712e079
Midhun G S <midhun.gs.contractor@move.com>
2020-05-04 02:29:04 +0530

    bug fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const validator = require('./lib/validator/validator');
-const checkService = require('./lib/validator/checkService');
+const checkService = require('./lib/checkService/checkService');
 
 module.exports = { validator, checkService };
